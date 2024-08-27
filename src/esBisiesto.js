c2262d9b740e1c41863e2511bf93dcc34087d522
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2024-08-27 03:48:53 -0400

    Todos los años divisibles por 100 pero no por 400 NO son años bisiestos

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,6 +1,11 @@
 function esBisiesto(año) {
     if (año % 400 === 0) {
       return true;
-}};
+    } else if (año % 100 === 0) {
+        return false;
+    }
+    
+    
+    };
 
 export default esBisiesto;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -5,4 +5,11 @@ describe("Función esBisiesto", () => {
       expect(esBisiesto(2000)).toEqual(true);
       expect(esBisiesto(1600)).toEqual(true);
     });
+
+    it("debería retornar false para los años divisibles por 100 pero no por 400", () => {
+        expect(esBisiesto(1700)).toEqual(false);
+        expect(esBisiesto(1800)).toEqual(false);
+        expect(esBisiesto(1900)).toEqual(false);
+        expect(esBisiesto(2100)).toEqual(false);
+      });
 });
